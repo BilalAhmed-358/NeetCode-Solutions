3267b0067988002d1d8a6cd048d6e8560d2251b2
BilalAhmed-358 <99674150+BilalAhmed-358@users.noreply.github.com>
2022-10-29 18:42:50 +0500

    Time: 40 ms (27.54%), Space: 15.5 MB (7.75%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -5,14 +5,17 @@ public:
         vector<vector<int>> freq (nums.size()+1);
         unordered_map<int,int> freq_holder;
         int count=0;
+        //O(N) time complexity
         for(int i=0;i<nums.size();i++)
         {
             freq_holder[nums[i]]++;
         }
+        //O(N) time complexity
         for(auto i:freq_holder)
         {
             freq[i.second].push_back(i.first);
         }
+        //2D Array traversal O(nums.size()+1 x freq[i].size) = O (N)
         for(int i=nums.size();i>=0;i--)
         {
                if(freq[i].size()!=0)
@@ -30,6 +33,8 @@ public:
                    }
                }
         }
+        
+        // overall time complexity O(N) + O(N) + O(N) = O (N)
         return ans;
     }
 };
