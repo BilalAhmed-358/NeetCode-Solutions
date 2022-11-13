5e0c778eae8108e094917bc892572df4c82c889d
BilalAhmed-358 <99674150+BilalAhmed-358@users.noreply.github.com>
2022-11-13 08:06:49 +0500

    Time: 55 ms (33.49%), Space: 20.4 MB (27.62%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<set<int>> rows(9), cols(9), blocks(9);
-        
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                
-                if (board[i][j] == '.') continue;
-                
-                int curr = board[i][j] - '0';
-                if (rows[i].count(curr) || cols[j].count(curr) || blocks[(i/3)*3+j/3].count(curr)) 
+    vector<set<int>> horizontal(9),vertical(9), box(9);
+     for(int i=0; i<board.size();i++)
+     {
+         for(int j=0;j<board[i].size();j++)
+         {
+            
+            if (board[i][j]=='.')
+                continue;
+             int curr=board[i][j]-'0';
+            // if(horizontal[i].count(curr)  || vertical[i].count(curr) || box[(i/3)*3+j/3].count(curr))
+             if (horizontal[i].count(curr) || vertical[j].count(curr) || box[(i/3)*3+j/3].count(curr)) 
                     return false;
-                
-                rows[i].insert(curr);
-                cols[j].insert(curr);
-                blocks[(i/3)*3+j/3].insert(curr);
-            }
-        }
-        
-        return true;
+             horizontal[i].insert(curr);
+             vertical[j].insert(curr);
+             box[(i/3)*3+j/3].insert(curr);
+         }
+         
+     }  
+    return true;    
     }
 };
