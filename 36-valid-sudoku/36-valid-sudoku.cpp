bbfbb0f68beffd0013d37efe679d6d4d77a28eeb
BilalAhmed-358 <99674150+BilalAhmed-358@users.noreply.github.com>
2022-11-14 01:37:19 +0500

    Time: 47 ms (52.63%), Space: 20.4 MB (27.38%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -10,8 +10,17 @@ public:
             if (board[i][j]=='.')
                 continue;
              int curr=board[i][j]-'0';
-             if (horizontal[i].count(curr) || vertical[j].count(curr) || box[(i/3)*3+j/3].count(curr)) 
-                    return false;
+             if (horizontal[i].count(curr) || vertical[j].count(curr) || box[(i/3)*3+j/3].count(curr))
+             {
+                 for(int i=0;i<horizontal.size();i++)
+                    {
+                        for(auto j:horizontal[i])
+                            cout<<j<<" ";
+                        cout<<endl;
+                    }
+                return false;
+                 
+             }
              horizontal[i].insert(curr);
              vertical[j].insert(curr);
              box[(i/3)*3+j/3].insert(curr);
